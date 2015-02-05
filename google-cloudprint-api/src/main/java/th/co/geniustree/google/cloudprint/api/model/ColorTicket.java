a92520931dae1fac9996492eff385e2cb5c5a5d1
Morten Larsson <mortenlarsson09@gmail.com>
2015-02-05 15:48:57 +0100

    Small error corrected

--- google-cloudprint-api/src/main/java/th/co/geniustree/google/cloudprint/api/model/ColorTicket.java
+++ google-cloudprint-api/src/main/java/th/co/geniustree/google/cloudprint/api/model/ColorTicket.java
@@ -11,7 +11,7 @@ package th.co.geniustree.google.cloudprint.api.model;
 public class ColorTicket {
 
     private String vendor_id;
-    private String type;
+    private int type;
 
     public String getVendor_id() {
         return vendor_id;
@@ -21,11 +21,11 @@ public class ColorTicket {
         this.vendor_id = vendor_id;
     }
 
-    public String getType() {
+    public int getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(int type) {
         this.type = type;
     }
 }
